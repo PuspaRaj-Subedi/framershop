7a91af6e3a58b99053e30b25d9edfeffba123b7d
pnthari <pnthari@gmail.com>
2020-10-06 14:54:37 +0545

    add new feature

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -41,35 +41,20 @@ class ProductController extends Controller
 
     }
 
-    public function store(Request $request)
+    public function store()
     {
         $products = new Product();
-        $medias = new Media();
-        $request->validate([
-            'product_name' => 'required|string',
-            'price' => 'required',
-            'description' => 'required',
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
-        ]);
-        if ($request->hasFile('image')) {
-            $title = $request->product_name;
-            $imageName = $title . time() . '.' . request()->image->getClientOriginalExtension();
-            request()->image->move(public_path('products'), $imageName);
-//            $medias->product_url = $imageName;
-//            $medias->save();
-            $products->product_id = $medias->id;
-            $products->Price = $request->price;
-            $products->user_id = Auth::user()->id;
-            $products->description = $request->description;
-            $products->product_name = $request->product_name;
-            $products->slug = str::slug($request->product_name, "-");
-            if ($products->save())
-                return response()->json(['data' => 'success'], $this->successStatus);
-            else
-                return response()->json(['error' => 'Unauthorised'], 401);
-        } else {
-            return response()->json(['error' => 'Error'], 401);
-        }
+        $products->product_name = request('product_name');
+        $products->Price = request('Price');
+        $products->user_id = Auth::id();
+        $products->description = request('description');
+        $products->image = request('image');
+
+        if ($products->save())
+            return response()->json(['data' => 'success'], $this->successStatus);
+        else
+            return response()->json(['error' => 'Unauthorised'], 401);
+
     }
 
     public function delete($product_id)
